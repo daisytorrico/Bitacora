0344182a8c89b0baf54b1d049494fff27c2bc11e
Daisy Torrico <daisytorrico@hotmail.com>
2026-05-25 14:47:38 -0300

    travel.kt con validacion de los campos y funcion de guardar en repository

--- app/src/main/java/com/catedra/bitacora/features/travel/domain/model/Travel.kt
+++ app/src/main/java/com/catedra/bitacora/features/travel/domain/model/Travel.kt
@@ -2,9 +2,17 @@ package com.catedra.bitacora.features.travel.domain.model
 
 import java.time.LocalDate
 
+/**
+ * Representa un viaje (Trip) en el sistema.
+ * Basado en las reglas de seguridad de Firestore.
+ */
 data class Travel(
-    val id: String,
+    val id: String = "",
     val name: String,
-    val startDate: LocalDate,
-    val endDate: LocalDate
+    val description: String = "",
+    val ownerId: String,
+    val imageUrl: String? = null,
+    val startDate: LocalDate? = null,
+    val endDate: LocalDate? = null,
+    val privileges: Map<String, String>? = null
 )
--- app/src/main/java/com/catedra/bitacora/features/travel/domain/repository/TravelsRepository.kt
+++ app/src/main/java/com/catedra/bitacora/features/travel/domain/repository/TravelsRepository.kt
@@ -3,7 +3,10 @@ package com.catedra.bitacora.features.travel.domain.repository
 import com.catedra.bitacora.features.travel.domain.model.Travel
 
 interface TravelsRepository {
-    suspend fun getTravels( userId: String, page: Int): Result<List<Travel>>;
+    suspend fun getTravels(userId: String, page: Int): Result<List<Travel>>
+    
+    // Guardar el viaje
+    suspend fun saveTravel(travel: Travel): Result<Unit>
 
     companion object {
         const val DEFAULT_PAGE_SIZE = 10
