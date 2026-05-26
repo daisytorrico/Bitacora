9f113f4d8bde3ce10b1a40758d0dff2b4d9d3b50
Daisy Torrico <daisytorrico@hotmail.com>
2026-05-26 17:25:07 -0300

    cambios menores

--- app/src/main/java/com/catedra/bitacora/features/travel/domain/useCase/GetGalleryIntentUseCase.kt
+++ app/src/main/java/com/catedra/bitacora/features/travel/domain/useCase/GetGalleryIntentUseCase.kt
@@ -5,10 +5,13 @@ import javax.inject.Inject
 
 class GetGalleryIntentUseCase @Inject constructor() {
     
-    operator fun invoke(): Intent {
+    operator fun invoke(allowMultiple: Boolean = false): Intent {
         return Intent(Intent.ACTION_GET_CONTENT).apply {
             type = "image/*"
             addCategory(Intent.CATEGORY_OPENABLE)
+            if (allowMultiple) {
+                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            }
         }
     }
 }
